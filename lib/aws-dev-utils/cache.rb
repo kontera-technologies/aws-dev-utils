f680cea131e6612962bb4d1c1a66ad9f129a993f
yardenmol <yardenmol12@gmail.com>
2019-08-08 11:34:01 +0300

    Update lib/aws-dev-utils/cache.rb
    Co-Authored-By: Eyal Shalev <eyalsh@gmail.com>

--- lib/aws-dev-utils/cache.rb
+++ lib/aws-dev-utils/cache.rb
@@ -17,7 +17,7 @@ module AwsDevUtils
     # If the key can't be found, the block will be run and its result returned and be set in the cache.
     # @param key [Object]
     # @param exp [Integer]
-    # @param block [block] - to excute
+    # @param block [block] - called with no arguments, and returns the new value for the cache (if no cached data is found).
     # @return [Object] the value from the cache or the result of the block
     def fetch key, exp=60, &block
       get(key) or block.().tap {|x| set(key, x, exp)}
