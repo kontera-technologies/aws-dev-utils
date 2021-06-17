c1cacbc383d90b4c70e24f049da1f4f9a70d4cb5
hagaigubber <hagai.gubber@amobee.com>
2021-06-17 12:58:36 +0300

    pr fixes

--- lib/aws-dev-utils/next_token_wrapper.rb
+++ lib/aws-dev-utils/next_token_wrapper.rb
@@ -14,18 +14,30 @@ module AwsDevUtils
     end
 
     private
-
+    #todo: add a condition for the route53 case and refactor var names
+    #irb -I lib of dev-utils
     def do_call m, *args, &block
       @client.send(m , *args, &block).to_h.tap do |r|
         i = 1
-        key, key2 = extract_keys r
-        while(key && r[key] && i < @max) do
-          i += 1
-          res = @client.send(m, (args[0]||{}).merge(key2 => r[key])).to_h
-          res.each { |k,v| r[k] = v.is_a?(Array) ? r[k].concat(v) : v }
-          r.delete_if {|k,v| !res.keys.include?(k) }
+        if r[:is_truncated] && r[:next_record_name]
+          while(r[:is_truncated].eql? true && i < @max) do
+            i += 1
+            res = @client.send(m, (args[0]||{}).merge(key2 => r[key])).to_h
+            res.each { |k,v| r[k] = v.is_a?(Array) ? r[k].concat(v) : v }
+            r.delete_if {|k,v| !res.keys.include?(k) }
+          end
+        else
+          key, key2 = extract_keys r #resp
+          # resp_key = key
+          # req_key = key2
+          while(key && r[key] && i < @max) do
+            i += 1
+            res = @client.send(m, (args[0]||{}).merge(key2 => r[key])).to_h
+            res.each { |k,v| r[k] = v.is_a?(Array) ? r[k].concat(v) : v }
+            r.delete_if {|k,v| !res.keys.include?(k) }
+          end
+          r.delete key
         end
-        r.delete key
       end
     end
 
@@ -43,3 +55,31 @@ module AwsDevUtils
 
   end
 end
+
+
+
+
+
+
+
+
+a = ["audience-profiler-svc-ui", "herodotus-api", "abi-mgmt", "first-party-uploader", "abi-realtime", "kw-suggest", "twitter-api", "abi-content-search"]
+a.map {}
+  next_name = "internal.aws.kontera.com."
+  next_type = "NS"
+  records = []
+  loop {
+    response = route53_client.list_resource_record_sets(
+      hosted_zone_id: "/hostedzone/Z15D6GJDGY0YD8",
+      start_record_name: next_name,
+      start_record_type: next_type,
+    )
+
+    records.push(response.resource_record_sets)
+    break unless response.is_truncated
+
+    next_name = response.next_record_name
+    next_type = response.next_record_type
+
+  }
+  records.flatten.
