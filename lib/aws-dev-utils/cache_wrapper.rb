1e697205c918bdef8b637603cf4bc51e4a2ab91f
Eyal Shalev <eyalsh@gmail.com>
2021-06-22 13:36:44 +0300

    Use client class name in cache key (#2)

--- lib/aws-dev-utils/cache_wrapper.rb
+++ lib/aws-dev-utils/cache_wrapper.rb
@@ -4,12 +4,13 @@ module AwsDevUtils
   class CacheWrapper
     include AwsDevUtils::Utils
 
-    # Initialize a new CacheWrapper, internal use only.
-    # @param client [Aws client, NextTokenWrapper, RetryWrapper]
-    # @param exp [Integer] - the key-value timeout
-    def initialize client, exp=60
+  # Initialize a new CacheWrapper, internal use only.
+  # @param client [Aws client, NextTokenWrapper, RetryWrapper]
+  # @param exp [Integer] - the key-value timeout
+  def initialize client, exp=60, client_name: nil
       @client = client
       @exp = exp
+      @client_name = client_name || client.class.name
     end
 
     def method_missing m, *args, &block
@@ -18,9 +19,13 @@ module AwsDevUtils
 
     private
 
-    def do_call m, args
-      Cache.instance.fetch([m, args.deep_sort, @exp], @exp) {
-        nested_hash(@client.send(m, args))
+    def cache_key m, params
+      [@client_name, m, params.deep_sort, @exp]
+    end
+
+    def do_call m, params
+      Cache.instance.fetch(cache_key(m, params), @exp) {
+        nested_hash(@client.send(m, params))
       }
     end
 
