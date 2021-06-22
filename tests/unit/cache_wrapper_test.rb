4e77790f2ece1d7c564d18adac24ed780134036d
Eyal Shalev <eyalsh@gmail.com>
2021-06-22 13:17:48 +0300

    .

--- tests/unit/cache_wrapper_test.rb
+++ tests/unit/cache_wrapper_test.rb
@@ -18,41 +18,41 @@ module AwsDevUtils
     def test_with_cache_set_value_without_args
       client = client_setup
       value = {res: 'test-value-from-cache'}
-      cache.set([:mock_func, {}, 60], value, 60)
+      cache.set(["test-client", :mock_func, {}, 60], value, 60)
       assert_equal value, client.mock_func
-      assert_equal value, cache.get([:mock_func, {}, 60])
+      assert_equal value, cache.get(["test-client", :mock_func, {}, 60])
     end
 
     def test_with_cache_set_value_with_args
       client = client_setup
       value = {res: 'test-value-from-cache'}
-      cache.set([:mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60], value, 60)
+      cache.set(["test-client", :mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60], value, 60)
       assert_equal value, client.mock_func({filters: [{name: 'filter1', value: ['val1', 'val2']}]})
-      assert_equal value, cache.get([:mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60])
+      assert_equal value, cache.get(["test-client", :mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60])
     end
 
     def test_with_cache_new_value_without_args
       client = client_setup
       value = {res: 'test-new-value'}
       assert_equal value, client.mock_func
-      assert_equal value, cache.get([:mock_func, {}, 60])
+      assert_equal value, cache.get(["test-client", :mock_func, {}, 60])
     end
 
     def test_with_cache_new_value_with_args
       client = client_setup
       value = {res: 'test-new-value'}
       assert_equal value, client.mock_func({filters: [{name: 'filter1', value: ['val1', 'val2']}]})
-      assert_equal value, cache.get([:mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60])
+      assert_equal value, cache.get(["test-client", :mock_func, {filters: [{name: 'filter1', value: ['val1', 'val2']}]}, 60])
     end
 
     def test_sort_filters
       client = client_setup
       value = {res: 'test-value-from-cache'}
       filters = {filters: [{name: 'filter2', value: ['val1', 'val2']}, {name: 'filter1', value: ['val2', 'val1']}]}
-      cache.set([:mock_func, filters.deep_sort, 60], value, 60)
+      cache.set(["test-client", :mock_func, filters.deep_sort, 60], value, 60)
       assert_equal value, client.mock_func(filters)
-      assert_equal value, cache.get([:mock_func, filters.deep_sort, 60])
-      assert_nil cache.get([:mock_func, filters, 60])
+      assert_equal value, cache.get(["test-client", :mock_func, filters.deep_sort, 60])
+      assert_nil cache.get(["test-client", :mock_func, filters, 60])
     end
 
     def cache
@@ -60,7 +60,7 @@ module AwsDevUtils
     end
 
     def client_setup
-      CacheWrapper.new MockClient.new
+      CacheWrapper.new MockClient.new, client_name: "test-client"
     end
 
     class MockClient
