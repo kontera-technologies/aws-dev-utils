97b54046696e06fa2608d63343554fd450b57332
hagaigubber <hagai.gubber@amobee.com>
2021-07-01 13:25:18 +0300

    remove comments

--- lib/aws-dev-utils/next_token_wrapper.rb
+++ lib/aws-dev-utils/next_token_wrapper.rb
@@ -9,15 +9,13 @@ module AwsDevUtils
       @max = max
     end
 
-    def method_missing m, *args, &block
-      do_call(m,*args,&block)
+    def method_missing method, *args, &block
+      do_call(method,*args,&block)
     end
 
     private
-    #todo: add a condition for the route53 case and refactor var names
-    #irb -I lib of dev-utils
-    def do_call m, props = {}, &block
-      @client.send(m, props, &block).to_h.tap do |response|
+    def do_call method, props = {}, &block
+      @client.send(method, props, &block).to_h.tap do |response|
         i = 1
         resp_keys, req_keys = extract_keys response
 
@@ -26,11 +24,20 @@ module AwsDevUtils
 
         while(!resp_keys.empty? && resp_keys.all?{ |resp_key| response[resp_key] } && i < @max) do
           i += 1
-          new_response = @client.send(m, props.merge( Hash[ req_keys.zip(response[resp_keys]) ] ) ).to_h
+          new_response = @client.send(
+            method,
+            props.merge(
+              Hash[
+                req_keys.zip(
+                  resp_keys.map { |resp_key| response[resp_key] }
+                )
+              ]
+            )
+          ).to_h
           new_response.each { |k,v| response[k] = v.is_a?(Array) ? response[k].concat(v) : v }
           response.delete_if { |k,v| !new_response.keys.include?(k) }
         end
-        response.delete resp_key
+        response.delete resp_keys
       end
     end
 
